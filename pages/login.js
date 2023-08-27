b195bc89a0b236a13629e67cfe89c8b3f33e7f17
oshara123 <amanthi1995@gmail.com>
2023-08-27 14:28:07 +0530

    added the login scripts for different user types

--- pages/login.js
+++ pages/login.js
@@ -1,9 +1,13 @@
+import { expect } from "@playwright/test";
 exports.LoginPage= class LoginPage {
     constructor(page){
         this.page=page;
         this.userName= page.locator('[data-test="username"]');
         this.password= page.locator('[data-test="password"]');
         this.loginBtn=page.locator('[data-test="login-button"]');
+
+        this.lockedOutUserError=page.locator('//div[@class="error-message-container error"]')
+        
     }
 
     async login(username,password){
@@ -12,4 +16,17 @@ exports.LoginPage= class LoginPage {
         await this.loginBtn.click();            
     }
 
+    async verifyLockedUserErrorMessage(){
+       await expect(this.lockedOutUserError).toContainText('Epic sadface: Sorry, this user has been locked out.');
+       
+    }
+
+    async verifyUserGotoHomePage(){
+        await expect(this.page).toHaveURL('https://www.saucedemo.com/inventory.html');
+
+    }
+
+    async verifyIncorrectUserNameErrorMessage(){
+        await expect(this.lockedOutUserError).toContainText('Epic sadface: Username and password do not match any user in this service');
+    }
 }
--- tests/loginScenarios.js
+++ tests/loginScenarios.js
@@ -8,9 +8,6 @@ test('Verify Standard User can login to the application successfully',async({pag
     const Login = new LoginPage(page);
 
     await Login.login("standard_user","secret_sauce");
-
-    
-
 })
 
 test('Verify Locked out user can login to the application',async({page})=>{
@@ -18,18 +15,42 @@ test('Verify Locked out user can login to the application',async({page})=>{
     const Login = new LoginPage(page);
 
     await Login.login("locked_out_user","secret_sauce");
+    await Login.verifyLockedUserErrorMessage();
 
 })
 
-// test("Verify problem user can login to the application",()=>{
+test("Verify problem user can login to the application",async({page})=>{
+    await page.goto('https://www.saucedemo.com/');
+    const Login = new LoginPage(page);
+    
+    await Login.login("problem_user","secret_sauce");
+    await Login.verifyUserGotoHomePage();
 
-// })
+})
 
-// test('Verify performance_glitch_user can login to the application',()=>{
+test('Verify performance_glitch_user can login to the application',async({page})=>{
+    await page.goto('https://www.saucedemo.com/');
+    const Login = new LoginPage(page);
 
-// })
+    await Login.login('performance_glitch_user','secret_sauce');
+    await Login.verifyUserGotoHomePage();
+})
 
-// test('Verify user cannot login to application with the correct user name & password',()=>{
+test('Verify user cannot login to application with a incorrect user name',async({page})=>{
 
-// })
+    await page.goto('https://www.saucedemo.com/');
+    const Login = new LoginPage(page);
+
+    await Login.login('user','secret_sauce');
+    await Login.verifyIncorrectUserNameErrorMessage();
+
+})
+
+test('Verify user cannot login to the application with a incorrect password',async({page})=>{
+    await page.goto('https://www.saucedemo.com/');
+    const Login = new LoginPage(page);
+    Login.login('standard_user','pass');
+    
+    await Login.verifyIncorrectUserNameErrorMessage();
+})
 }
