7cb30d20a414db57e479bbd26764878dc73babd9
oshara123 <amanthi1995@gmail.com>
2023-09-10 17:20:10 +0530

    add beforeEach for buying products script

--- tests/buyProduct.js
+++ tests/buyProduct.js
@@ -5,32 +5,61 @@ import { ShoppingCartPage } from '../pages/shoppingCart';
 import { YourInformationPage } from '../pages/yourInformation';
 import { CheckoutPage } from '../pages/checkout';
 import {CheckoutCompletePage} from '../pages/checkoutComplete';
+import { url } from '../environment-config';
+import { firstName, lastName, postalCode } from '../data-config';
 
 export const BuyProducts=()=>{
+
+    test.beforeEach(async({page})=>{
+        const Login = new LoginPage(page);
+        await page.goto(url);
+        await Login.login("standard_user","secret_sauce");
+
+    })
     test('Verify user can buy a single product successfully',async({page})=>{
     
-    const Login = new LoginPage(page);
     const ProductObj = new ProductPage(page);
     const ShoppingCartObj = new ShoppingCartPage(page);
     const YourInforObj= new YourInformationPage(page);
     const CheckoutObj= new CheckoutPage(page);
     const CompletePageObj= new CheckoutCompletePage(page)
 
-
-    await page.goto('https://www.saucedemo.com/');
-    await Login.login("standard_user","secret_sauce");
-
     await ProductObj.addProduct();
     await ProductObj.clickShoppingCart();
     await ShoppingCartObj.validatingTheAddedProduct();
     await ShoppingCartObj.checkoutProduct();
-    await YourInforObj.fillPersonalDetails("Sam","Smith","40000");
+    await YourInforObj.fillPersonalDetails(firstName,lastName,postalCode);
     await YourInforObj.clickContinue();
     await CheckoutObj.verifyCheckoutProductName();
     await CheckoutObj.clickFinish();
     await CompletePageObj.verfiyThankYouText();
     await CompletePageObj.clickBackToHomeBtn();
 
+    })
 
-})
+    test('Verify User can buy mulitiple products successfully',async({page})=>{
+
+        const ProductObj =new ProductPage(page);
+        const ShoppingCartObj = new ShoppingCartPage(page);
+        const YourInforObj = new YourInformationPage(page);
+        const CheckoutObj = new CheckoutPage(page);
+        const CompletePageObj = new CheckoutCompletePage(page);
+
+        await ProductObj.addProduct();
+        await ProductObj.addBikeProduct();
+        await ProductObj.clickShoppingCart();
+        await ShoppingCartObj.validatingTheAddedProduct();
+        await ShoppingCartObj.validatingAddedBikeTitle();
+        await ShoppingCartObj.checkoutProduct();
+        await YourInforObj.fillPersonalDetails(firstName,lastName,postalCode);
+        await YourInforObj.clickContinue();
+        await CheckoutObj.verifyCheckoutProductName();
+        await CheckoutObj.verifyBikeTitleName();
+        await CheckoutObj.clickFinish();
+        await CompletePageObj.clickBackToHomeBtn();
+
+    })
+
+
+
 }
